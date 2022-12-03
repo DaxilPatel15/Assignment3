711e4bdd2d69ae767fd6eeeab14a76226ffebdd6
DaxilPatel15 <114842131+DaxilPatel15@users.noreply.github.com>
2022-12-03 17:03:47 -0500

    Update view.php

--- Form/view.php
+++ Form/view.php
@@ -38,25 +38,23 @@ if(isset($_GET['deletedID']) && !empty($_GET['deletedID'])){
     <main>
       <?php
       // adding msg statement to give infromation about the update,inserted and deleted data.
-      if(isset($_GET['msg1']) == "insert"){
-        echo "<div class='alert alert-success alert-dismissible'>
-             <button type='button' class='close' data-dismiss='alert'>X</button>
-             Your profile has been added!
-             </div>";
+      if (isset($_GET['msg1']) == "insert") {
+      echo "<div class='alert alert-success alert-dismissible'>
+              <button type='button' class='close' data-dismiss='alert'>×</button>
+              Your Registration added successfully
+            </div>";
       }
-
-      if(isset($_GET['msg2']) == "update"){
+      if (isset($_GET['msg2']) == "update") {
         echo "<div class='alert alert-success alert-dismissible'>
-             <button type='button' class='close' data-dismiss='alert'>X</button>
-             Your profile has been updated!
-             </div>";
+              <button type='button' class='close' data-dismiss='alert'>×</button>
+              Your Registration updated successfully
+            </div>";
       }
-
-      if(isset($_GET['msg3']) == "delete"){
-        echo "<div class='alert alert-success alert-dismissible'>
-             <button type='button' class='close' data-dismiss='alert'>X</button>
-             Your profile has been deleted!
-             </div>";
+      if (isset($_GET['msg3']) == "delete") {
+       echo "<div class='alert alert-success alert-dismissible'>
+              <button type='button' class='close' data-dismiss='alert'>×</button>
+              Record deleted successfully
+            </div>";
       }
        ?>
 
